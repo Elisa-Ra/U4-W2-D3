81a3e6e26b58c401f36a510abbddd9ada6757637
Elisa Raeli <79461498+Elisa-Ra@users.noreply.github.com>
2026-01-14 14:39:37 +0100

    adds files

---
+++ u4-w2-d3/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // CREARE TRE CLASSI
+        //SOLO ARRAY LIST, NO SET ECC...
+        // non usare faker su tutto
+
+    }
+}
